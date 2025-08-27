33a00171ac393c204b217870471206697b969dd8
Vicenzo <Vicenzux@gmail.com>
2025-08-27 16:50:50 -0300

    inicio da adição do salto e obstáculos imóveis

---
+++ Assets/Script/Obstacle.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player colidiu com um obstaculo");
+        }
+    }
+}
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,10 +8,14 @@ public class PlayerMovement : MonoBehaviour
     public float routeDistance = 1f;
     int route = 0;
     public float speed = 1;
+    bool isJumping;
+    public float JumpForce = 10;
+    Rigidbody rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -24,7 +29,9 @@ public class PlayerMovement : MonoBehaviour
         {
             route++;
         }
+        
         SideDash();
+        
         // Reset for testing
         if (Input.GetKeyDown(KeyCode.R))
         {
@@ -32,8 +39,27 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            isJumping = true;
+            Jump();
+        }
+        if (transform.position.y <= 1.05)
+        {
+            isJumping = false;
+        }
+    }
+
     void SideDash()
     {
-        transform.position = Vector3.MoveTowards(transform.position, new Vector3(route * routeDistance, transform.position.y, transform.position.z), speed * Time.deltaTime);
+        //transform.position = Vector3.MoveTowards(transform.position, new Vector3(route * routeDistance, transform.position.y, transform.position.z), speed * Time.deltaTime);
+        transform.Translate(route * routeDistance, transform.position.y, transform.position.z);
+    }
+    
+    void Jump()
+    {
+
     }
 }
