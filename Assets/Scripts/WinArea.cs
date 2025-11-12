20a5f83d55c3fa85119b659e983d8eacac3a24bd
akanibal <akanibaldev@gmail.com>
2025-11-12 08:33:40 -0300

    o player caminha ate a paquera no final

--- Assets/Scripts/WinArea.cs
+++ Assets/Scripts/WinArea.cs
@@ -1,22 +1,64 @@
 using UnityEngine;
+using System.Collections;
 
 public class WinArea : MonoBehaviour
 {
     public UIController uiController;
+    public GameObject paqueraF;
+    public GameObject paqueraM;
+    public float moveSpeed = 3f;
+    public float stopDistance = 1f;
 
-    public void Start()
+    private bool isMovingToPaquera = false;
+    private GameObject player;
+    private GameObject targetPaquera;
+
+    void Start()
     {
         uiController = FindFirstObjectByType<UIController>();
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !isMovingToPaquera)
         {
-            if (uiController != null)
+            player = other.gameObject;
+            isMovingToPaquera = true;
+
+            var playerController = player.GetComponent<Player>();
+            if (playerController != null)
+                playerController.enabled = false;
+
+            if (paqueraF != null && paqueraF.activeInHierarchy)
+                targetPaquera = paqueraF;
+            else if (paqueraM != null && paqueraM.activeInHierarchy)
+                targetPaquera = paqueraM;
+            else
             {
-                uiController.Win();
+                Debug.LogWarning("Nenhuma paquera ativa encontrada!");
+                return;
             }
+
+            StartCoroutine(MoveToPaquera());
+        }
+    }
+
+    IEnumerator MoveToPaquera()
+    {
+        while (Vector3.Distance(player.transform.position, targetPaquera.transform.position) > stopDistance)
+        {
+            Vector3 direction = (targetPaquera.transform.position - player.transform.position).normalized;
+            player.transform.position += direction * moveSpeed * Time.deltaTime;
+
+            Vector3 lookPos = new Vector3(targetPaquera.transform.position.x, player.transform.position.y, targetPaquera.transform.position.z);
+            player.transform.LookAt(lookPos);
+
+            yield return null;
+        }
+
+        if (uiController != null)
+        {
+            uiController.Win();
         }
     }
 }
