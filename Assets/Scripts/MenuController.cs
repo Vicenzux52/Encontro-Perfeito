028d1a2cb0a3d4e33997c2484de34451a20432f9
akanibal <akanibaldev@gmail.com>
2025-11-12 02:40:58 -0300

    menu programado
    falta testar o audio

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,6 +3,9 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
     public GameObject audioPanel;
+    public GameObject controlsPanel;
+    public GameObject creditsPanel;
+
     public void Play()
     {
         SceneManager.LoadScene(1);
@@ -18,9 +21,21 @@ public class MenuController : MonoBehaviour
         audioPanel.SetActive(true);
     }
 
+    public void Controls()
+    {
+        controlsPanel.SetActive(true);
+    }
+
+    public void Credits()
+    {
+        creditsPanel.SetActive(true);
+    }
+
     public void BackButton()
     {
         audioPanel.SetActive(false);
+        controlsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
     }
 
     void Start()
