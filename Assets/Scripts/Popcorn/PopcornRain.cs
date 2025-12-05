e46af0bbe2cbc1ebdaccb088fb9b1f3cedc5156f
K <168924521+Kai-Ofc@users.noreply.github.com>
2025-12-05 19:30:57 -0300

    algumas mudanças

--- Assets/Scripts/Popcorn/PopcornRain.cs
+++ Assets/Scripts/Popcorn/PopcornRain.cs
@@ -15,7 +15,7 @@ public class PopcornRain : MonoBehaviour
         if (player.position.z < 1370 && player.position.z > 1052 && spawnCounter > spawnDelay)
         {
             transform.position = 
-            Vector3.right * transform.position.x + Vector3.up * transform.position.y + Vector3.forward * (player.position.z + 30);
+            Vector3.right * transform.position.x + Vector3.up * transform.position.y + Vector3.forward * (player.position.z + 25);
             Destroy(Instantiate(popcorn, transform.position + Vector3.forward* Random.Range(-6, 6), Quaternion.identity), DeadTime);
             spawnCounter = 0;
         }
